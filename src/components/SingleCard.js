1b02830c6b05da02959d4f536e81bb4314daa530
GurTati <tatiana.gurova.j@gmail.com>
2024-01-25 12:52:03 +0000

    create card component

---
+++ src/components/SingleCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./SingleCard.css"
+
+export default function SinglrCard ({ card }) {
+
+  return (
+    <div className='card'>
+        <div>
+            <img src={card.src} alt="card front" className="front" />
+            <img src="/img/cover.png" alt="card back" className="back" />
+        </div>
+    </div>
+  )
+}
