1cd82a8638566936d2d98a18d3faa672069556cd
GurTati <tatiana.gurova.j@gmail.com>
2024-01-26 13:01:24 +0000

    matching and flipping cards

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./SingleCard.css"
 
-export default function SingleCard ({ card, handleChoice }) {
+export default function SingleCard ({ card, handleChoice, flipped }) {
 
   const handleClick = () => {
     handleChoice(card)
@@ -9,7 +9,7 @@ export default function SingleCard ({ card, handleChoice }) {
 
   return (
     <div className='card'>
-        <div>
+        <div className={flipped ? 'flipped' : ''}>
             <img src={card.src} alt="card front" className="front" />
             <img 
                 src="/img/cover.png" 
